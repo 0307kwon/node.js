44f122d9035b18d7de99baed8bd741545d62ab69
권세진 <0307kwon@gmail.com>
2020-09-27 17:19:48 +0900

    리팩토링, 보안문제 handle

--- main.js
+++ main.js
@@ -2,114 +2,14 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 const querystring = require("querystring");
-const { report } = require("process");
+const templeteObj = require("./lib/templeteObj.js");
+const redirectObj = require("./lib/redirectObj.js");
+const path = require("path");
+const preprocessObj = require("./lib/preprocessObj.js");
 
 
-function getTemplete(title,list_templete,contents,control){
-    return `
-    <!doctype html>
-    <html>
-    <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-    </head>
-    <body>
-    <h1><a href="/">WEB</a></h1>
-    <ol>
-        ${list_templete}
-    </ol>
-    ${control}
-    <h2>${title}</h2>
-    ${contents}
-    </body>
-    </html>
-    `;
-}
-
-function getListTemplete(folder_list){
-    let list_templete = "";
-    for(let i=0; i<folder_list.length; i++){
-        const title = folder_list[i];
-        list_templete +=`<li><a href="/?id=${title}">${title}</a></li>`
-    }    
-    return list_templete;
-}
-
-function redirectCreatePostQuery(request,response){
-    let body = "";
-
-    request.on('data', chunk => {
-        body += chunk.toString();
-    });
-
-    request.on('end' , () => {   
-        body = querystring.parse(body);
-        fs.writeFile(`./nodejs/post/${body.title}`,`${body.contents}`,"utf8", (err) => {
-            if(err) throw err;
-            console.log(body.title);
-            response.writeHead(302,{Location:`/?id=${encodeURIComponent(body.title)}`});
-            response.end();
-
-        });
-
-    });
-}
-
-function getCreateTemplete(){
-    return `
-    <form action="/process_create" method="POST">
-        <p><input type="text" name="title" placeholder="제목을 입력하세요"></p>
-        <p>
-            <textarea name=contents placeholder="내용을 입력하세요"></textarea>
-        </p>
-        <p>
-            <input type="submit">
-        </p>
-    </form>
-    `;
-}
-
-function getUpdateTemplete(title_origin,contents_origin){
-    return `
-    <form action="/process_update" method="POST">
-    <input type="hidden" name="title_origin" value="${title_origin}">
-    <p><input type="text" name="title_revision" placeholder="수정할 제목을 입력하세요." value="${title_origin}"></p>
-    <p><textarea name="contents" placeholder="수정할 내용을 입력하세요">${contents_origin}</textarea></p>    
-    <p><input type="submit"></p>
-    </form>
-    `;
-}
-
-function redirectUpdatePostQuery(request,response){
-    let body = "";
-
-    request.on('data', chunk => {
-        body += chunk.toString();
-    });
-
-    request.on('end' , () => {   
-        body = querystring.parse(body);
-        fs.rename(`./nodejs/post/${body.title_origin}`, `./nodejs/post/${body.title_revision}`, function (err) {
-            if (err) throw err;
-            
-            fs.writeFile(`./nodejs/post/${body.title_revision}`, body.contents, function (err) {
-                if (err) throw err;
-                console.log('Replaced!');
-                response.writeHead(302,{Location:`/?id=${encodeURIComponent(body.title_revision)}`});
-                response.end();
-              });
-          });
-          
-    });
-}
-
-function getDeleteTemplete(){
-    return `
-    <p>게시글이 삭제되었습니다.</p>
-    <form action="/">
-    <input type="submit" value="확인">
-    </form>
-    `;
+function secureQuery(data){
+    return path.parse(data);
 }
 
 const app = http.createServer(function(request,response){
@@ -118,57 +18,64 @@ const app = http.createServer(function(request,response){
     const pathname =url.parse(url_requested,true).pathname;
     const folder = "./nodejs/post";
 
-    let title,control;
+    let control,titleInQueryData;
     fs.readdir(folder,(err, folder_list) => {
         if(err) throw err;
 
         response.writeHead(200);
         
-        title = queryData.id;
+        titleInQueryData = queryData.id;
+        if(titleInQueryData !== undefined){
+            titleInQueryData = secureQuery(queryData.id).base;
+        }
+        
         control = `<a href="/create">create</a>`;
-        const list_templete = getListTemplete(folder_list);
+        const list_templete = templeteObj.getList(folder_list);
         if(pathname === '/'){
-            if(title === undefined){
-                title = "welcome";
+            const sanitizedTitle = preprocessObj.cleanText(titleInQueryData);
+            if(sanitizedTitle === "undefined"){
+                const title = "welcome";
                 const welcomeContents = "hi friend";
-                const templete = getTemplete(title,list_templete,welcomeContents,control);
+                const templete = templeteObj.getEntire(title,list_templete,welcomeContents,control);
                 response.end(templete);
-            }else{
-                fs.readFile(`nodejs/post/${title}`,"utf8",(err, fileContents) => {
-                    control += ` <a href="/update?id=${title}">update</a>`;
-                    control += ` <a href="/delete?id=${title}">delete</a>`;
-                    const templete = getTemplete(title,list_templete,fileContents,control);
+            }else{ // read
+                fs.readFile(`nodejs/post/${sanitizedTitle}`,"utf8",(err, fileContents) => {
+                    if(err) throw err;
+                    
+                    control += ` <a href="/update?id=${sanitizedTitle}">update</a>`;
+                    control += ` <a href="/delete?id=${sanitizedTitle}">delete</a>`;
+                    const templete = templeteObj.getEntire(sanitizedTitle,list_templete,fileContents,control);
                     response.end(templete);
                 });
             }
         }else if(pathname === "/create"){
-            title = "Create Post";
-            const Createcontents = getCreateTemplete();
-            const templete = getTemplete(title,list_templete,Createcontents,``);
+            const title = "Create Post";
+            const Createcontents = templeteObj.getCreate();
+            const templete = templeteObj.getEntire(title,list_templete,Createcontents,``);
             response.end(templete);
         }else if(pathname === "/process_create"){
             if(request.method === "POST"){  
-                redirectCreatePostQuery(request,response);
+                redirectObj.createPost(request,response);
             }
         }else if(pathname === "/update"){
-            title = "Update Post";
-            title_origin = queryData.id;
+            const title = "Update Post";
+            const title_origin = titleInQueryData;
             fs.readFile(`nodejs/post/${title_origin}`,"utf8",(err, fileContents) => {
-            const createContents = getUpdateTemplete(title_origin,fileContents);
-            const templete = getTemplete(title,list_templete,createContents,``);
+            const createContents = templeteObj.getUpdate(title_origin,fileContents);
+            const templete = templeteObj.getEntire(title,list_templete,createContents,``);
             response.end(templete);
             });
         }else if(pathname === "/process_update"){
             if(request.method === "POST"){  
-                redirectUpdatePostQuery(request,response);
+                redirectObj.updatePost(request,response);
             }
         }else if(pathname === "/delete"){
-            title = queryData.id;
+            const title = titleInQueryData;
             fs.unlink(`nodejs/post/${title}`, function (err) {
                 if (err) throw err;
                 console.log('File deleted!');
-                const deleteContents = getDeleteTemplete();
-                const templete = getTemplete(``,list_templete,deleteContents,``);
+                const deleteContents = templeteObj.getDelete();
+                const templete = templeteObj.getEntire(``,list_templete,deleteContents,``);
                 response.end(templete);
               });
         }else{
